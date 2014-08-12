050308d7590e87161356f53ea75e4e764273d1b0
Darkin8 <darkin8@gmail.com>
2014-08-12 23:26:50 +0300

    Update init-data.sql

--- init-data.sql
+++ init-data.sql
@@ -1,16 +1,18 @@
+create database `main`;
+use `main`;
+
 create table `human` (
 	human_id int not null primary key,
 	name varchar(32),
-	brain_id int not null unique) enigne=INNODB;
+	brain_id int not null unique) enigne InnoDB;
 
 create table `brains` (
 	brain_id int not null primary key,
-	foreign key (brain_id) references human(brain_id) on delete cascade) engine=INNODB;
+	iq_level int,
+	foreign key (brain_id) references human(brain_id) on delete cascade) engine InnoDB;
 
 create table `ideas` (
 	idea_id int not null primary key,
 	content text,
 	brain_id int not null,
-	foreign key (brain_id) references brains(brain_id) on delete cascade) engine=INNODB;
-
-
+	foreign key (brain_id) references brains(brain_id) on delete cascade) engine InnoDB;
