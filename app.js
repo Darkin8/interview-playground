fd1d10a06aa871b8220b48b3ef00e9d83e4c15ac
Darkin8 <darkin8@gmail.com>
2014-08-12 21:42:38 +0000

    Simple service #1

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 var express = require('express');
+var mysql = require('mysql');
+
 var app = express();
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '12345'
+});
+
+connection.connect();
+
+app.get('/', function(req, res) {
+	connection.query("select human.human_id as id, human.name as name, count(ideas.idea_id) as num_ideas from human left join ideas on (human.brain_id = ideas.brain_id) group by id limit 100",
+		function (err, rows, fields) {
+			if (err) {
+				console.log("DB read error");
+				throw err;
+			}
+
+			var result = "";
+			if (rows != null && rows.length > 0) {
+				for (var i=0; i<rows.lenth; i++) {
+					result += rows[i].name + " (" + rows[i].num_ideas + ")\n";
+				}
+			}
+
+			res.send(result);
+		});
+});
+
+app.listen(3000);
--- init-data.sql
+++ init-data.sql
@@ -2,17 +2,57 @@ create database `main`;
 use `main`;
 
 create table `human` (
-	human_id int not null primary key,
+	human_id int auto_increment primary key,
 	name varchar(32),
+<<<<<<< HEAD
 	brain_id int not null unique) enigne InnoDB;
 
 create table `brains` (
 	brain_id int not null primary key,
+=======
+	brain_id int not null unique) engine InnoDB;
+
+create table `brains` (
+	brain_id int auto_increment primary key,
+>>>>>>> 62a41da... Simple service #1
 	iq_level int,
 	foreign key (brain_id) references human(brain_id) on delete cascade) engine InnoDB;
 
 create table `ideas` (
-	idea_id int not null primary key,
+	idea_id int auto_increment primary key,
 	content text,
 	brain_id int not null,
 	foreign key (brain_id) references brains(brain_id) on delete cascade) engine InnoDB;
+<<<<<<< HEAD
+=======
+
+insert into `human`
+	(name, brain_id) values
+	("Aristotle", 1),("Socrates", 2),("Einstein", 3), ("George W. Bush", 4);
+
+insert into `brains`
+	(brain_id, iq_level) values
+	(1, 150), (2, 160), (3, 170), (4, 50);
+
+insert into `ideas`
+	(brain_id, content) values
+	(1, "Love is composed of a single soul inhabiting two bodies."),
+	(1, "It is the mark of an educated mind to be able to entertain a thought without accepting it."),
+	(1, "The roots of education are bitter, but the fruit is sweet."),
+	(2, "I know that I am intelligent, because I know that I know nothing."),
+	(2, "By all means, marry. If you get a good wife, you'll become happy; if you get a bad one, you'll become a philosopher."),
+	(2, "The only true wisdom is in knowing you know nothing."),
+	(2, "Beware the barrenness of a busy life."),
+	(2, "Wisdom begins in wonder."),
+	(2, "He is richest who is content with the least, for content is the wealth of nature."),
+	(3, "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning."),
+	(3, "You have to learn the rules of the game. And then you have to play better than anyone else."),
+	(3, "Try not to become a man of success, but rather try to become a man of value."),
+
+	(3, "The difference between stupidity and genius is that genius has its limits."),
+	(3, "We cannot solve our problems with the same thinking we used when we created them."),
+	(4, "I want to thank you for taking time out of your day to come and witness my hanging."),
+	(4, "It's clearly a budget. It's got a lot of numbers in it."),
+	(4, "I have a different vision of leadership. A leadership is someone who brings people together."),
+	(4, "I just want you to know that, when we talk about war, we're really talking about peace.");
+>>>>>>> 62a41da... Simple service #1
