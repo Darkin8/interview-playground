fb13454231883670d74c659e81c524286c1ca5ed
Darkin8 <darkin8@gmail.com>
2014-08-13 02:57:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var connection = mysql.createConnection({
 	password: '12345'
 });
 
+var validator = require('validator');
+
 connection.connect();
 
 app.get('/', function(req, res) {
@@ -18,14 +20,45 @@ app.get('/', function(req, res) {
 				throw err;
 			}
 
-			var result = "";
+			var result = "/	            - list of authors\n" +
+				     "/[author_id]  - list of ideas\n\n";
 			if (rows != null && rows.length > 0) {
 				for (var i=0; i<rows.length; i++) {
-					result += rows[i].name + " (" + rows[i].num_ideas + ")\n";
+					result += rows[i].name + " (" + rows[i].num_ideas + ")" +
+						"[id:" + rows[i].id + "]\n";
 				}
 			}
 
 			res.send(result);
+		});
+});
+
+app.get('/:author_id', function(req, res) {
+	var id = req.param('author_id');
+	if(validator.isInt(id)) {
+		id = validator.toInt(id);
+	} else {
+		res.redirect('/');
+		return;
+	}
+	
+	connection.query("select human.name as name, ideas.content as content from ideas left join human on (ideas.brain_id = human.brain_id) where human.human_id = ?", [id],
+		function(err, rows, fields) {
+			if (err) {
+				console.log("DB read error");
+				throw err;
+			}
+			
+			if (rows != null && rows.length > 0) {
+				var result = "Great ideas of {" + rows[0].name + "}\n\n";
+				for (var i=0; i<rows.length; i++) {
+					result += i + ". " + rows[i].content + "\n";
+				}
+				
+				res.send(result);
+			} else {
+				res.redirect('/');
+			}
 		});
 });
 
